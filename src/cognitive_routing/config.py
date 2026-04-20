b94dcc368cabb3af629f962924f7c405421b3aa4
peter-luminova <Pankaj.kumar@luminova.ai>
2026-04-20 07:20:27 +0530

    Lower default routing threshold

--- src/cognitive_routing/config.py
+++ src/cognitive_routing/config.py
@@ -1,6 +1,5 @@
 MODEL_NAME = "BAAI/bge-small-en-v1.5"
 CHROMA_COLLECTION_NAME = "bot_personas"
-DEFAULT_THRESHOLD = 0.85
+DEFAULT_THRESHOLD = 0.55
 CHROMA_HOST = "http://chromadb:8000"
 CHROMA_PERSIST_DIR = "./chroma_data"
-
